f244ff8032117e24d2b11aff8b10a527a7c5cca3
Ankitgif <ankitsinha2303@gmail.com>
2020-04-15 14:16:54 +0530

    [Ankit] Added test for handling exception

--- test_MoodAnalyserr.py
+++ test_MoodAnalyserr.py
@@ -7,7 +7,12 @@ class Test_MoodAnalyserrTest:
         mood = moodAnalyser.analyseMood("This is a sad message")
         assert mood == 'SAD'
 
-    def test_givenMessage_WhenSad_ShouldReturnSad(self):
+    def test_givenMessage_WhenHappy_ShouldReturnHappy(self):
         moodAnalyser = MoodAnalyserr()
         mood = moodAnalyser.analyseMood("This is a happy message")
-        assert mood == 'HAPPY'    
+        assert mood == 'HAPPY'
+
+    def test_givenMessage_WhenNone_ShouldReturnHappy(self):
+        moodAnalyser = MoodAnalyserr()
+        mood = moodAnalyser.analyseMood(None)
+        assert mood == 'HAPPY'        
